57d11a750c68087d622debec8170c1f93af7e075
Xololt <105434979+xololt-dev@users.noreply.github.com>
2023-12-05 22:38:48 +0100

    Begin work on simulatedAnnealing

--- src/simulatedAnnealing.cpp
+++ src/simulatedAnnealing.cpp
@@ -8,5 +8,57 @@ void Algorithms::simulatedAnnealing(Matrix* matrix) {
 	std::vector<short> currentSolutionOrder = vertexOrder, bestSolutionOrder = vertexOrder;
 	int currentSolutionLength = pathLength, bestSolutionLength = pathLength;
 
+	generateRandomCandidate(&currentSolutionOrder, currentNeighbourhoodType);
+}
 
+std::vector<short> Algorithms::generateRandomCandidate(std::vector<short>* currentOrder, NeighbourhoodType nearType) {
+	std::vector<short> returnVector;
+
+	switch (nearType)
+	{
+	case INVERSE:
+		returnVector = inverse(currentOrder);
+		break;
+	case SWAP:
+		returnVector = swap(currentOrder);
+		break;
+	case INSERT:
+		returnVector = insert(currentOrder);
+		break;
+	case INSERT_SUB:
+		returnVector = insertSub(currentOrder);
+		break;
+	default:
+		break;
+	}
+
+	return returnVector;
+}
+
+std::vector<short> Algorithms::inverse(std::vector<short>* currentOrder) {
+	std::vector<short> returnVector;
+	returnVector.reserve(currentOrder->size());
+
+	return returnVector;
+}
+
+std::vector<short> Algorithms::swap(std::vector<short>* currentOrder) {
+	std::vector<short> returnVector;
+	returnVector.reserve(currentOrder->size());
+
+	return returnVector;
+}
+
+std::vector<short> Algorithms::insert(std::vector<short>* currentOrder) {
+	std::vector<short> returnVector;
+	returnVector.reserve(currentOrder->size());
+
+	return returnVector;
+}
+
+std::vector<short> Algorithms::insertSub(std::vector<short>* currentOrder) {
+	std::vector<short> returnVector;
+	returnVector.reserve(currentOrder->size());
+
+	return returnVector;
 }
